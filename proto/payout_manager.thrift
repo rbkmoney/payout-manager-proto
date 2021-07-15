6e228e9e61922985c264e621db2d8e7b2e79ef82
inallyoung <in.all.young@gmail.com>
2021-07-15 16:52:27 +0300

    JD-493: Added new params

--- proto/payout_manager.thrift
+++ proto/payout_manager.thrift
@@ -1,17 +1,18 @@
 include "base.thrift"
 include "domain.thrift"
-include "msgpack.thrift"
 
 namespace java com.rbkmoney.payout.manager
 namespace erlang payout_manager
 
 typedef base.ID PayoutID
 typedef base.SequenceID SequenceID
+typedef base.Timestamp Timestamp
+typedef base.InvalidRequest InvalidRequest
 
 struct Event {
     1: required PayoutID payout_id
     2: required SequenceID sequence_id
-    3: required base.Timestamp created_at
+    3: required Timestamp created_at
     4: required PayoutChange payout_change
     5: required Payout payout
 }
@@ -27,7 +28,7 @@ struct PayoutCreated {
 
 struct Payout {
     1: required PayoutID payout_id
-    2: required base.Timestamp created_at
+    2: required Timestamp created_at
     3: required domain.PartyID party_id
     4: required domain.ShopID shop_id
     5: required PayoutStatus status
@@ -82,20 +83,29 @@ struct PayoutCancelled {
  */
 struct PayoutConfirmed {}
 
-
-exception PayoutNotFound {}
+exception NotFound {
+    1: required string message
+}
 
 /* Когда на счете для вывода недостаточно средств */
 exception InsufficientFunds {}
+
+exception PayoutAlreadyExists {}
 
 /**
 * Параметры для создания выплаты
 * shop - параметры магазина
 * amount - сумма выплаты
+* payout_id - уникальный идентификатор выплаты.
+* Если не указан, то сгенерируется новый.
+* payout_tool_id - идентификатор типа выплаты.
+* Если не указан, то будет взят из параметров магазина
 **/
 struct PayoutParams {
     1: required ShopParams shop_params
     2: required domain.Cash cash
+    3: optional PayoutID payout_id
+    4: optional domain.PayoutToolID payout_tool_id
 }
 
 struct ShopParams {
@@ -108,22 +118,25 @@ service PayoutManagement {
     /**
      * Создать выплату на определенную сумму и платежный инструмент
      */
-    Payout CreatePayout (1: PayoutParams payout_params)
-        throws (1: InsufficientFunds ex1, 2: base.InvalidRequest ex2)
+    Payout CreatePayout (1: PayoutParams payout_params) throws (
+        1: InsufficientFunds ex1,
+        2: InvalidRequest ex2,
+        3: PayoutAlreadyExists ex3,
+        4: NotFound ex4)
 
     /**
     * Получить выплату по идентификатору
     */
-    Payout GetPayout (1: PayoutID payout_id) throws (1: PayoutNotFound ex1)
+    Payout GetPayout (1: PayoutID payout_id) throws (1: NotFound ex1)
 
     /**
      * Подтвердить выплату.
      */
-    void ConfirmPayout (1: PayoutID payout_id) throws (1: base.InvalidRequest ex1)
+    void ConfirmPayout (1: PayoutID payout_id) throws (1: InvalidRequest ex1)
 
     /**
      * Отменить движения по выплате.
      */
-    void CancelPayout (1: PayoutID payout_id, 2: string details) throws (1: base.InvalidRequest ex1)
+    void CancelPayout (1: PayoutID payout_id, 2: string details) throws (1: InvalidRequest ex1)
 
 }
